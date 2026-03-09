8ef65c64c759d8e50da132987f531328aebbaf56
binyul <jiibinyul@gmail.com>
2026-03-09 17:32:03 +0545

    complete delete user listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utilis/error.js";
 
 
 export const createListing = async (req, res,next) => {
@@ -9,5 +10,20 @@ export const createListing = async (req, res,next) => {
     } catch (error) {
         next(error);
     }
-
 };  
+
+export const deleteListing = async (req, res, next) => {
+    const listing= await Listing.findById(req.params.id);
+    if(!listing){
+        return next(errorHandler(404, "Listing not found"));
+    }
+    if (req.user.id !== listing.userRef){
+        return next(errorHandler(401, "You are not authorized to delete this listing"));
+    }
+    try {
+        await Listing.findByIdAndDelete(req.params.id);
+        res.status(200).json("Listing deleted successfully");
+    } catch (error) {
+        next(error);
+    }
+}
